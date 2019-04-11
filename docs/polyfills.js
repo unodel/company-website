3659009de7bb9761dd97945aba979acdfeab39a1
Manish Patel <manishtpatel@yahoo.com>
2019-04-11 07:57:53 -0400

    changed name

--- docs/polyfills.js
+++ docs/polyfills.js
@@ -3152,7 +3152,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\GitHub\unodel-company-website\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/manish/bitbucket/unodel_website/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
